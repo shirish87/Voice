55e3ca5411bfe4e3c7251b3b080119bda4d87b7d
Shirish Kamath <shirishk.87@gmail.com>
2022-03-01 23:46:40 +0530

    Enable setting book category

--- app/src/main/kotlin/voice/app/features/bookOverview/EditBookBottomSheetController.kt
+++ app/src/main/kotlin/voice/app/features/bookOverview/EditBookBottomSheetController.kt
@@ -2,27 +2,45 @@ package voice.app.features.bookOverview
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import com.bluelinelabs.conductor.Controller
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.squareup.anvil.annotations.ContributesTo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import voice.app.databinding.BookMoreBottomSheetBinding
 import voice.app.features.bookmarks.BookmarkController
 import voice.app.misc.RouterProvider
 import voice.app.misc.conductor.asTransaction
+import voice.bookOverview.BookOverviewCategory
+import voice.bookOverview.category
 import voice.common.conductor.DialogController
+import voice.core.AppScope
+import voice.core.rootComponentAs
 import voice.data.Book
 import voice.data.getBookId
 import voice.data.putBookId
+import voice.data.repo.BookRepository
+import javax.inject.Inject
 
 class EditBookBottomSheetController(args: Bundle) : DialogController(args) {
 
+  @Inject
+  lateinit var bookRepo: BookRepository
+
   private val bookId = args.getBookId(NI_BOOK)!!
 
+  init {
+    rootComponentAs<Component>().inject(this)
+  }
+
   override fun onCreateDialog(savedViewState: Bundle?): Dialog {
-    val dialog = BottomSheetDialog(activity!!)
+    val book = runBlocking { bookRepo.get(bookId) }
+      ?: error("Cannot instantiate ${javaClass.name} without a current book")
 
     val binding = BookMoreBottomSheetBinding.inflate(activity!!.layoutInflater)
-    dialog.setContentView(binding.root)
-
     binding.title.setOnClickListener {
       val router = (activity as RouterProvider).provideRouter()
       EditBookTitleDialogController(bookId).showDialog(router)
@@ -43,8 +61,70 @@ class EditBookBottomSheetController(args: Bundle) : DialogController(args) {
 
       dismissDialog()
     }
+    binding.markAsNotStarted.apply {
+      visibility = if (book.category == BookOverviewCategory.NOT_STARTED) View.GONE else View.VISIBLE
 
-    return dialog
+      setOnClickListener {
+        val firstChapter = book.chapters.first()
+
+        lifecycleScope.launch {
+          bookRepo.updateBook(bookId) {
+            it.copy(
+              currentChapter = firstChapter.id,
+              positionInChapter = 0,
+            )
+          }
+
+          withContext(Dispatchers.Main) {
+            dismissDialog()
+          }
+        }
+      }
+    }
+    binding.markAsCurrent.apply {
+      visibility = if (book.category == BookOverviewCategory.CURRENT) View.GONE else View.VISIBLE
+
+      setOnClickListener {
+        val firstChapter = book.chapters.first()
+
+        lifecycleScope.launch {
+          bookRepo.updateBook(bookId) {
+            it.copy(
+              currentChapter = firstChapter.id,
+              positionInChapter = 1,
+            )
+          }
+
+          withContext(Dispatchers.Main) {
+            dismissDialog()
+          }
+        }
+      }
+    }
+    binding.markAsComplete.apply {
+      visibility = if (book.category == BookOverviewCategory.FINISHED) View.GONE else View.VISIBLE
+
+      setOnClickListener {
+        val lastChapter = book.chapters.last()
+
+        lifecycleScope.launch {
+          bookRepo.updateBook(bookId) {
+            it.copy(
+              currentChapter = lastChapter.id,
+              positionInChapter = lastChapter.duration,
+            )
+          }
+
+          withContext(Dispatchers.Main) {
+            dismissDialog()
+          }
+        }
+      }
+    }
+
+    return BottomSheetDialog(activity!!).apply {
+      setContentView(binding.root)
+    }
   }
 
   private fun callback() = targetController as Callback
@@ -67,5 +147,10 @@ class EditBookBottomSheetController(args: Bundle) : DialogController(args) {
   interface Callback {
     fun onInternetCoverRequested(book: Book.Id)
     fun onFileCoverRequested(book: Book.Id)
+  }
+
+  @ContributesTo(AppScope::class)
+  interface Component {
+    fun inject(target: EditBookBottomSheetController)
   }
 }
